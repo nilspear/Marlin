824060410be6cd3c63cb953d6ccaaf31125acbe1
nilspear <59541708+nilspear@users.noreply.github.com>
2020-07-01 17:05:07 -0400

    center bed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1361,8 +1361,8 @@
 
 // Manually set the home position. Leave these undefined for automatic settings.
 // For DELTA this is the top-center of the Cartesian print volume.
-#define MANUAL_X_HOME_POS -3
-#define MANUAL_Y_HOME_POS -14
+#define MANUAL_X_HOME_POS  -2.5
+#define MANUAL_Y_HOME_POS -13
 //#define MANUAL_Z_HOME_POS 0
 
 // Use "Z Safe Homing" to avoid homing with a Z probe outside the bed area.
